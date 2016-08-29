7e85c4393ab2736cd1d7db389830f28c9b9afce3
Jordan Wright <jwright028@student.butte.edu>
2016-08-29 02:06:57 +0000

    Lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,12 @@
+//Jordan Wright
+//8/25/16
+//A random number is selected and the user is asked for an input guess.
+
+// int main() {
+ // computer chooses number between 1 and 20
+ // loop{
+ // user is asked to make a guess
+ // if guess is high print out "too high". -1 point
+ // elseif guess is low print out "too low". -1 point
+ // else print "you win". +10 points. break loop.}
+ //}
