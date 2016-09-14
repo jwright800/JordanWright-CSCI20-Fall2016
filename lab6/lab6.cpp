390669395f5bfed2680574d3503218715f8dd01f
Jordan Wright <jwright028@student.butte.edu>
2016-09-14 22:11:50 +0000

    Lab 6 Submission

---
+++ lab6/lab6.cpp
@@ -0,0 +1,53 @@
+/* 
+    Jordan Wright
+    9/14/16
+    A mad libs game that gets user input and outputs a paragragh.
+    Now with copyright!
+*/
+
+#include <iostream> 
+#include <string>
+using namespace std;
+
+void Copyright()
+{
+    cout << "    *   "  << endl;
+    cout << "   ***  "  << endl;
+    cout << "  ***** "  << "Copyright Jordan Wright 2016" << endl;
+    cout << "   ***  "  << endl;
+    cout << "    *   "  << endl;
+
+    return;
+}
+
+int main()
+{
+    int number;
+    float number2;
+    char character;
+    string dessert;
+    
+    cout << "Enter a number between 2 an 50." << endl;
+    cin >> number;
+    cout << endl << "Enter another number between 5 and 6" << endl;
+    cin >> number2;
+    cout << endl << "Enter a character." << endl;
+    cin >> character;
+    cout << endl << "Enter the name of a dessert" << endl;
+    cin >> dessert;
+    
+    cout << endl << "Number 1: " << number << endl;
+    cout << "Number 2: " << number2 << endl;
+    cout << "Character: " << character << endl;
+    cout << "Dessert: " << dessert << endl;
+    
+    cout << endl << "Johnny knew he couldn't stay awake forever. It'd had been a whole "
+         << number << " hours since he last slept.";
+    cout << " Last time he counted " << number2 << " aliens. I'm not even sure that's possible.";
+    cout << " Last time their leader, he called himself Mr." << character << ", said they came from planet "
+         << dessert << "." << endl << endl;
+    
+    Copyright();
+
+
+}
