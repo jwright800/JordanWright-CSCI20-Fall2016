68df45cf4ac7bccfb8f051ab92cf9c492a4220e5
Jordan Wright <jwright028@student.butte.edu>
2016-10-18 17:20:21 +0000

    Lab 14 submission

---
+++ lab14/lab14.cpp
@@ -0,0 +1,37 @@
+/* 
+    Jordan Wright
+    10/18/16
+*/
+
+ // This program reads in a letter and reports whether// it is an uppercase letter, a lowercase letter, or neither.
+
+ #include <iostream>
+ using namespace std; 
+ 
+ int main() {       
+     // Read a character in
+     char ch;
+     
+     cout << "Please enter a character: ";
+     cin  >> ch;         // check -- is it a letter??
+     
+    if (ch >= 'A' && ch <= 'Z') {
+        cout << "Yes, that is a uppercase letter." << endl;
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        cout << "Yes, that is a lowercase letter" << endl;
+    }
+    else {
+        cout << "Not a letter" << endl;
+    }
+    return 0; }
+ 
+/*Correct output
+Please enter a character: F
+Yes, that is an uppercase letter
+Please enter a character: f
+Yes, that is a lowercase letter
+Please enter a character: Z
+Yes, that is an uppercase letter
+Please enter a character: 3
+Not a letter */
